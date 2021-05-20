0b9539f6af867d32c48a6d9d53e0a9ea44c77dd7
Saga <sagaswahn@hotmail.com>
2021-05-20 20:19:09 +0200

    Encrypt password before saving it to database

--- api/models/User.js
+++ api/models/User.js
@@ -1,9 +1,17 @@
 const mongoose = require("mongoose");
 
+const Encrypt = require("../Encrypt");
+
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
+});
+
+// Encrypt password before saving it to database.
+userSchema.pre("save", async function (next) {
+  this.password = Encrypt.encrypt(this.password);
+  return next();
 });
 
 const User = mongoose.model("User", userSchema);
