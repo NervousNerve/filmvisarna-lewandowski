0271f65664c5506319333cf267f5a00f798381a6
Sebastian Mineur <sebastian.mineur@gmail.com>
2021-05-20 13:42:38 +0200

    Added URI for mongoDB

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const app = express();
 const port = 3001;
 const uri =
+  "mongodb+srv://dbAdmin:password1234@cluster0.bzbxx.mongodb.net/filmvisarna?retryWrites=true&w=majority";
 
 const movieRoutes = require("./routes/movieRoutes");
 
