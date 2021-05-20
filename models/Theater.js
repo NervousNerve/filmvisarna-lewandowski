ee5b83945a5ac3339efde508d6e1e5e6d97306fd
Sebastian Mineur <sebastian.mineur@gmail.com>
2021-05-20 10:44:08 +0200

    Create Theater model

---
+++ models/Theater.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const movieSchema = mongoose.Schema({
+  title: { type: String },
+  price: { type: Number },
+  runtime: { type: Number },
+  rated: { type: String },
+  genre: [{ type: String }],
+  actors: [{ type: String }],
+  director: { type: String },
+  language: [{ type: String }],
+  plot: { type: String },
+  imageUrl: { type: String },
+  trailerUrl: { type: String },
+});
+
+const Movie = mongoose.model("Movie", movieSchema);
+
+module.exports = Movie;
