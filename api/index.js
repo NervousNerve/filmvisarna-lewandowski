1e16e3d9894fe669774073bcb41a76f845ab1cbb
Sebastian Mineur <sebastian.mineur@gmail.com>
2021-05-21 09:35:27 +0200

    Create bookings route

--- api/index.js
+++ api/index.js
@@ -8,11 +8,13 @@ const uri =
 
 const movieRoutes = require("./routes/movieRoutes");
 const theaterRoutes = require("./routes/theaterRoutes");
+const bookingRoutes = require("./routes/bookingRoutes");
 
 app.use(express.json());
 
 app.use("/api/v1/movies", movieRoutes);
 app.use("/api/v1/theaters", theaterRoutes);
+app.use("/api/v1/bookings", bookingRoutes);
 
 (async () => {
   try {
