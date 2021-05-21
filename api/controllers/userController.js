b3863d227584910ba4d1197acaa98662a0b00238
Saga <sagaswahn@hotmail.com>
2021-05-21 10:34:38 +0200

    Created login function and route

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const Encrypt = require("../Encrypt");
 
 const getAllUsers = async (req, res) => {
   let users = await User.find().exec();
@@ -28,7 +29,24 @@ const createUser = async (req, res) => {
   return res.json(user);
 };
 
+const login = async (req, res) => {
+  let userExists = await User.exists({ email: req.body.email });
+
+  if (userExists) {
+    let user = await User.findOne({ email: req.body.email }).exec();
+
+    req.body.password = Encrypt.encrypt(req.body.password);
+    if (user.password === req.body.password) {
+      user.password = undefined;
+      return res.json({ message: "Login successfull", loggedInUser: user });
+    }
+  }
+
+  return res.status(401).json({ error: "Bad credentials" });
+};
+
 module.exports = {
   createUser,
   getAllUsers,
+  login,
 };
