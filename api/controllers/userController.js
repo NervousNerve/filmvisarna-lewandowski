141fd7ea5e67fed57a38a490548df1b92a813e4b
Saga <sagaswahn@hotmail.com>
2021-05-21 10:14:05 +0200

    Added getAllUsers and route

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,5 +1,19 @@
 const User = require("../models/User");
 
+const getAllUsers = async (req, res) => {
+  let users = await User.find().exec();
+  if (users.length === 0) {
+    return res.status(404).json({ error: "Users are nowhere to be found" });
+  }
+
+  users = users.map((user) => {
+    // Object.assign merges two or more objects together. It takes everything from the second object (and forward) and puts it or updates it in the first argument. I set password to undefined because undefined cant be stringified, it will therefore be removed or trashed.
+    return Object.assign(user, { password: undefined });
+  });
+
+  return res.json(users);
+};
+
 const createUser = async (req, res) => {
   let userExists = await User.exists({ email: req.body.email });
 
@@ -16,4 +30,5 @@ const createUser = async (req, res) => {
 
 module.exports = {
   createUser,
+  getAllUsers,
 };
