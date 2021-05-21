ca2057663c0c0db33c0eeb5229d079bde72d77c3
Saga <sagaswahn@hotmail.com>
2021-05-21 10:37:19 +0200

    Removed getAllUsers function
    Was created for testing purpose

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,20 +1,6 @@
 const User = require("../models/User");
 const Encrypt = require("../Encrypt");
 
-const getAllUsers = async (req, res) => {
-  let users = await User.find().exec();
-  if (users.length === 0) {
-    return res.status(404).json({ error: "Users are nowhere to be found" });
-  }
-
-  users = users.map((user) => {
-    // Object.assign merges two or more objects together. It takes everything from the second object (and forward) and puts it or updates it in the first argument. I set password to undefined because undefined cant be stringified, it will therefore be removed or trashed.
-    return Object.assign(user, { password: undefined });
-  });
-
-  return res.json(users);
-};
-
 const createUser = async (req, res) => {
   let userExists = await User.exists({ email: req.body.email });
 
