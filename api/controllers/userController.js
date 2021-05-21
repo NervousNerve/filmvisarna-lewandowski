adda87a9ec5704676ab2ffa2130f262b9d00d262
Saga <sagaswahn@hotmail.com>
2021-05-21 14:58:02 +0200

    Created whoamI

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,17 +1,19 @@
 const User = require("../models/User");
 const Encrypt = require("../Encrypt");
 
+const whoami = (req, res) => {
+  return res.json(req.session.user || null);
+};
+
 const createUser = async (req, res) => {
   let userExists = await User.exists({
-    email: req.body.email
+    email: req.body.email,
   });
 
   if (userExists) {
-    return res
-      .status(400)
-      .json({
-        error: "A user with that email already exists"
-      });
+    return res.status(400).json({
+      error: "A user with that email already exists",
+    });
   }
 
   let user = await User.create(req.body);
@@ -21,12 +23,12 @@ const createUser = async (req, res) => {
 
 const login = async (req, res) => {
   let userExists = await User.exists({
-    email: req.body.email
+    email: req.body.email,
   });
 
   if (userExists) {
     let user = await User.findOne({
-      email: req.body.email
+      email: req.body.email,
     }).exec();
 
     req.body.password = Encrypt.encrypt(req.body.password);
@@ -34,17 +36,18 @@ const login = async (req, res) => {
       user.password = undefined;
       return res.json({
         message: "Login successfull",
-        loggedInUser: user
+        loggedInUser: user,
       });
     }
   }
 
   return res.status(401).json({
-    error: "Bad credentials"
+    error: "Bad credentials",
   });
 };
 
 module.exports = {
+  whoami,
   createUser,
   login,
 };
--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
 
+router.get("/whoami", userController.whoami);
 router.post("", userController.createUser);
 router.post("/login", userController.login);
 
