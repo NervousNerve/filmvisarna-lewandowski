b6f4f4c40f7b33548e9c51a2b74d67ed8ce1395a
Saga <sagaswahn@hotmail.com>
2021-05-21 16:26:36 +0200

    Create function to set current user

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -1,9 +1,31 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const UserContext = createContext();
 
 const UserProvider = (props) => {
-  const values = {};
+  const [currentUser, setCurrentUser] = useState(undefined);
+
+  useEffect(() => {
+    whoami();
+  }, []);
+
+  const whoami = async () => {
+    let user = await fetch("/api/v1/users/whoami", {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify(),
+    });
+    user = await user.json();
+
+    if (user.success) {
+      setCurrentUser(user);
+      console.log(currentUser);
+    } else if (user.error) {
+      console.log(user.error);
+    }
+  };
+
+  const values = { currentUser };
 
   return (
     <UserContext.Provider value={values}>{props.children}</UserContext.Provider>
