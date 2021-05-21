92bc0b57469c66f027dcedc0880c87c36eb5d3e2
Henrik <henrik.lood.mail@gmail.com>
2021-05-21 13:21:07 +0200

    Testing and merging createUser and login
    Removed getAllUsers from export.modules
    with Amanda

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -2,12 +2,16 @@ const User = require("../models/User");
 const Encrypt = require("../Encrypt");
 
 const createUser = async (req, res) => {
-  let userExists = await User.exists({ email: req.body.email });
+  let userExists = await User.exists({
+    email: req.body.email
+  });
 
   if (userExists) {
     return res
       .status(400)
-      .json({ error: "A user with that email already exists" });
+      .json({
+        error: "A user with that email already exists"
+      });
   }
 
   let user = await User.create(req.body);
@@ -16,23 +20,31 @@ const createUser = async (req, res) => {
 };
 
 const login = async (req, res) => {
-  let userExists = await User.exists({ email: req.body.email });
+  let userExists = await User.exists({
+    email: req.body.email
+  });
 
   if (userExists) {
-    let user = await User.findOne({ email: req.body.email }).exec();
+    let user = await User.findOne({
+      email: req.body.email
+    }).exec();
 
     req.body.password = Encrypt.encrypt(req.body.password);
     if (user.password === req.body.password) {
       user.password = undefined;
-      return res.json({ message: "Login successfull", loggedInUser: user });
+      return res.json({
+        message: "Login successfull",
+        loggedInUser: user
+      });
     }
   }
 
-  return res.status(401).json({ error: "Bad credentials" });
+  return res.status(401).json({
+    error: "Bad credentials"
+  });
 };
 
 module.exports = {
   createUser,
-  getAllUsers,
   login,
 };
