224341a355ee7c2506e5eef35eb11ec0f133dc52
Saga <sagaswahn@hotmail.com>
2021-05-21 09:27:19 +0200

    Created register function

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,3 +1,19 @@
 const User = require("../models/User");
 
-module.exports = {};
+const createUser = async (req, res) => {
+  let userExists = await User.exists({ email: req.body.email });
+
+  if (userExists) {
+    return res
+      .status(400)
+      .json({ error: "A user with that email already exists" });
+  }
+
+  let user = await User.create(req.body);
+  user.password = undefined;
+  return res.json(user);
+};
+
+module.exports = {
+  createUser,
+};
