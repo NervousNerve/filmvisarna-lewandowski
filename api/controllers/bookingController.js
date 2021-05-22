1c2879e69960549334680a369c52e832d0eed9c1
Sebastian Mineur <sebastian.mineur@gmail.com>
2021-05-22 11:54:21 +0200

    Populate Screening

--- api/controllers/bookingController.js
+++ api/controllers/bookingController.js
@@ -1,8 +1,12 @@
 const Booking = require("../models/Booking");
+// Screening is needed for populate
+const Screening = require("../models/Screening");
 
 const getBookingById = async (req, res) => {
   try {
-    const booking = await Booking.findOne({ _id: req.params.id }).exec();
+    const booking = await Booking.findOne({ _id: req.params.id })
+      .populate("screeningId")
+      .exec();
 
     if (!booking) {
       return res.status(404).json({ error: "No such booking found" });
