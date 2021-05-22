d486570d0e0d0fe4f8765dcebcc7ca48d14cf9b5
Sebastian Mineur <sebastian.mineur@gmail.com>
2021-05-22 10:51:12 +0200

    Insert BrowserRouter in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { BrowserRouter } from "react-router-dom";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter></BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
