360c28c7ef326b725351dee8b46dfbdfa52f775d
Sebastian Mineur <sebastian.mineur@gmail.com>
2021-05-22 11:50:05 +0200

    Create booking controller

--- api/routes/bookingRoutes.js
+++ api/routes/bookingRoutes.js
@@ -1,4 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const bookingController = require("../controllers/bookingController");
+
+router.get("/:id", bookingController.getBookingById);
 
 module.exports = router;
