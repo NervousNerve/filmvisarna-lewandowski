66a8a7b93a2119b80ea83789545ebcbfee7b74c4
Sebastian Mineur <sebastian.mineur@gmail.com>
2021-05-22 11:50:39 +0200

    Create getBookingById function

--- api/controllers/bookingController.js
+++ api/controllers/bookingController.js
@@ -1,3 +1,15 @@
 const Booking = require("../models/Booking");
 
-module.exports = {};
+const getBookingById = async (req, res) => {
+  try {
+    const booking = await Booking.findOne({ _id: req.params.id }).exec();
+    res.json(booking);
+  } catch (err) {
+    console.error(err);
+    return res.status(400).json({ error: err.message });
+  }
+};
+
+module.exports = {
+  getBookingById,
+};
