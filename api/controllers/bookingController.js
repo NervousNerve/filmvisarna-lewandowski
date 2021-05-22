d12b0b93807ce88c547e90ed4166bd9b1c7933ba
Sebastian Mineur <sebastian.mineur@gmail.com>
2021-05-22 11:52:12 +0200

    Handle no booking found

--- api/controllers/bookingController.js
+++ api/controllers/bookingController.js
@@ -3,6 +3,11 @@ const Booking = require("../models/Booking");
 const getBookingById = async (req, res) => {
   try {
     const booking = await Booking.findOne({ _id: req.params.id }).exec();
+
+    if (!booking) {
+      return res.status(404).json({ error: "No such booking found" });
+    }
+
     res.json(booking);
   } catch (err) {
     console.error(err);
