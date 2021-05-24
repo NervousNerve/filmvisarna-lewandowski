bef12782d679f24f3ff9d9ec97ef08df686e4f9b
Saga <sagaswahn@hotmail.com>
2021-05-24 13:20:27 +0200

    Created toggle menu for login/register

--- src/components/Entry.js
+++ src/components/Entry.js
@@ -4,7 +4,25 @@ import { useState } from "react";
 import styles from "./css/Entry.module.css";
 
 const Entry = () => {
-  return <div></div>;
+  const [message, setMessage] = useState(false);
+  const [menu, setMenu] = useState(false);
+
+  const toggleMenu = (e) => {
+    setMessage(!message);
+    setMenu(!menu);
+  };
+
+  return (
+    <div>
+      {menu ? <Login /> : <Register />}
+
+      <p onClick={() => toggleMenu()}>
+        {message
+          ? "Not a user? Register here!"
+          : "Already user? Please log in!"}
+      </p>
+    </div>
+  );
 };
 
 export default Entry;
