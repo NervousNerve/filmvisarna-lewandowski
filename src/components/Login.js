af877feeb5da494a4816fa0aa3bd74c8065684d2
Saga <sagaswahn@hotmail.com>
2021-05-24 13:09:14 +0200

    Create login component

---
+++ src/components/Login.js
@@ -0,0 +1,28 @@
+import styles from "./css/Login.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLock, faUser } from "@fortawesome/free-solid-svg-icons";
+
+const Login = () => {
+  return (
+    <div>
+      <h1>Log in</h1>
+      <p>You have to log in to book tickets.</p>
+
+      <div>
+        <FontAwesomeIcon icon={faUser} />
+        <input placeholder="email" type="email" />
+      </div>
+
+      <div>
+        <FontAwesomeIcon icon={faLock} />
+        <input placeholder="password" type="password" />
+      </div>
+
+      <button>Log in</button>
+
+      <p>Forgot your password?</p>
+    </div>
+  );
+};
+
+export default Login;
