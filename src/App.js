7ca18b926065b9b09f9bf5867f311145322c3aba
Lisa Hansson <lisastina.hansson@gmail.com>
2021-05-24 13:08:57 +0200

    Comments

--- src/App.js
+++ src/App.js
@@ -6,6 +6,21 @@ function App() {
     <div className="App">
       <UserProvider>
         <BrowserRouter></BrowserRouter>
+        <input type="text" className="search" placeholder="Search..." />
+        <input type="text" />
+        <button>Submit</button>
+        <div className="custom-select">
+          <select name="" id="">
+            <option value="">Imorgon 22:00</option>
+            <option value="">imorgom</option>
+            <option value="">onsdag</option>
+          </select>
+        </div>
+        <div className="number-input">
+        <button className="counter"></button>
+          <input className="quantity" min="0" name="quantity" type="number"/>
+          <button className="plus counter"></button>
+        </div>
       </UserProvider>
     </div>
   );
