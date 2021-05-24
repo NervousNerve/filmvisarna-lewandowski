aadcf790207fbb0b32bcbc36b47b97aba7cf0ada
Henrik <henrik.lood.mail@gmail.com>
2021-05-24 10:51:33 +0200

    Refactured code and tested with Postman
    Function to get all bookings by id now works
    With Amanda

--- api/controllers/bookingController.js
+++ api/controllers/bookingController.js
@@ -1,20 +1,25 @@
 const Booking = require("../models/Booking.js");
 
 const getBookingsByUser = async (req, res) => {
-  // //might have to be changed depending on how things go
-  let idToFind = req.body._id;
+  let idToFind = req.query.userId;
 
-  if (err) {
-    Booking.find({ userId: idToFind }).exec((err, bookings) => {
-      res.status(400).json({ error: "Something went wrong" });
+  Booking.find({
+    userId: idToFind
+  }).exec((err, bookings) => {
+    if (err) {
+      res.status(400).json({
+        error: "Something went wrong"
+      });
       return;
-    });
-  }
-  if (!bookings) {
-    res.json({ message: `No bookings to show` });
-    return;
-  }
-  res.json(bookings);
+    }
+    if (!bookings) {
+      res.json({
+        message: `No bookings to show`
+      });
+      return;
+    }
+    res.json(bookings);
+  });
 };
 
 module.exports = {
