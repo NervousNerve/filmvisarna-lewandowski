df8212c0b7c4bc41f455b4be06e80d0544d92044
Sebastian Mineur <sebastian.mineur@gmail.com>
2021-05-24 15:32:29 +0200

    Reorder routes by specificity

--- api/routes/bookingRoutes.js
+++ api/routes/bookingRoutes.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 const bookingController = require("../controllers/bookingController");
 
-router.get("/:id", bookingController.getBookingById);
 router.get("/userbookings", bookingController.getBookingsByUser);
+router.get("/:id", bookingController.getBookingById);
 router.post("/", bookingController.createBooking);
 
 module.exports = router;
