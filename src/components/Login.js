680761eb92a6916a49a9ffcfa62d511fa60e8953
Saga <sagaswahn@hotmail.com>
2021-05-24 16:05:46 +0200

    Add onChange-methods to login fields

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,20 +1,37 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLock, faUser } from "@fortawesome/free-solid-svg-icons";
 import styles from "../css/Login.module.css";
+import { useState, useContext } from "react";
+import { UserContext } from "../contexts/UserContext";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  /* const { login } = useContext(UserContext); */
+
   return (
     <div>
       <h1>Log in</h1>
       <p>You have to log in to book tickets.</p>
       <form className={styles.placeholder}>
         <FontAwesomeIcon icon={faUser} className={styles.userIcon} />
-        <input placeholder="Email" type="email" />
+        <input
+          placeholder="Email"
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
 
         <FontAwesomeIcon icon={faLock} className={styles.lockIcon} />
-        <input placeholder="Password" type="password" />
+        <input
+          placeholder="Password"
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
 
-        <button>Log in</button>
+        <button onClick={() => Login()}>Log in</button>
       </form>
     </div>
   );
