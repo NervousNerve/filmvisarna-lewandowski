4605be49d807c27e2bdedb48d7b790c8935ba99b
Amanda <amandaelizaolsson@gmail.com>
2021-05-24 10:24:32 +0200

    Changed route
    With Henrik

--- api/routes/bookingRoutes.js
+++ api/routes/bookingRoutes.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const bookingController = require("../controllers/bookingController");
 
-router.get("/bookings", bookingController.getBookingsByUser);
+router.get("/userbookings", bookingController.getBookingsByUser);
 
 module.exports = router;
