dfa47737e658da99ab3e8a16388f5b947219c4a3
Saga <sagaswahn@hotmail.com>
2021-05-24 11:19:20 +0200

    Rewriting functions with try and catch

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -11,40 +11,48 @@ const createUser = async (req, res) => {
   });
 
   if (userExists) {
-    return res.status(400).json({
-      error: "A user with that email already exists",
-    });
+    return res
+      .status(400)
+      .json({ error: "A user with that email already exists" });
   }
 
-  let user = await User.create(req.body);
-  user.password = undefined;
-  return res.json(user);
+  if (!req.body.email || !req.body.name || !req.body.password) {
+    return res.status(401).json({ error: "Something is missing in request." });
+  }
+
+  try {
+    let user = await User.create(req.body);
+    user.password = undefined;
+    return res.json(user);
+  } catch {
+    return res.status(401).json({ error: "Something went wrong." });
+  }
 };
 
 const login = async (req, res) => {
-  let userExists = await User.exists({
-    email: req.body.email,
-  });
-
-  if (userExists) {
+  try {
     let user = await User.findOne({
       email: req.body.email,
     }).exec();
 
-    req.body.password = Encrypt.encrypt(req.body.password);
-    if (user.password === req.body.password) {
-      delete user.password;
-      req.session.user = user;
-      return res.json({
-        message: "Login successfull",
-        loggedInUser: user,
-      });
+    if (!user) {
+      return res.status(401).json({ error: "Bad credentials." });
     }
-  }
 
-  return res.status(401).json({
-    error: "Bad credentials",
-  });
+    req.body.password = Encrypt.encrypt(req.body.password);
+    if (user.password !== req.body.password) {
+      return res.status(401).json({ error: "Bad credentials." });
+    }
+
+    delete user.password;
+    req.session.user = user;
+    return res.json({
+      message: "Login successfull",
+      loggedInUser: user,
+    });
+  } catch {
+    return res.json({ error: "Bad credentials." });
+  }
 };
 
 const logout = (req, res) => {
