39904889bf27baaf626fef079b388203b78debea
Amanda <amandaelizaolsson@gmail.com>
2021-05-24 14:26:58 +0200

    Added toggle function for displaying upcoming or previous bookings

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -17,7 +17,9 @@ const ProfilePage = () => {
     <div className="profilepage">
       <h1>Hi user </h1>
       <h2>My bookings:</h2>
-      <h3></h3>
+      <h3 onClick={() => toggleBookings()}>Previous</h3>
+      <h3 onClick={() => toggleBookings()}>Upcoming</h3>
+      {displayUpcoming ? <p>display upcoming</p> : <p>Display previous</p>}
     </div>
   );
 };
