562c1e3170773f6c709c29e973941d3b17f64ef8
Amanda <amandaelizaolsson@gmail.com>
2021-05-24 14:11:20 +0200

    Imported userContext to profilepage

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,5 +1,25 @@
+import { useContext, useEffect, useState } from "react";
+import { UserContext } from "../contexts/UserContext";
+
 const ProfilePage = () => {
-  return <div></div>;
+  const { currentUser } = useContext(UserContext);
+  const [displayPreviousBookings, setDisplayPreviousBookings] = useState(false);
+
+  const toggleBookings = () => {
+    if (displayPreviousBookings) {
+      setDisplayPreviousBookings(true);
+    } else {
+      setDisplayPreviousBookings(false);
+    }
+  };
+
+  return (
+    <div className="profilepage">
+      <h1>Hi {currentUser.name} </h1>
+      <h2>My bookings:</h2>
+      <h3></h3>
+    </div>
+  );
 };
 
 export default ProfilePage;
