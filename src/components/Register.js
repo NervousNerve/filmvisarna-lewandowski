69a6cd2c567c8859aa2f09aca331e5361b3f2f60
Saga <sagaswahn@hotmail.com>
2021-05-24 19:29:21 +0200

    Create handleRegister

--- src/components/Register.js
+++ src/components/Register.js
@@ -5,9 +5,31 @@ const Register = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [feedbackMessage, setFeedbackMessage] = useState(null);
+
   const { setUserToRegister } = useContext(UserContext);
 
-  const handleRegister = (e) => {};
+  const handleRegister = (e) => {
+    e.preventDefault();
+    const regex = new RegExp(
+      "^(?=.{6,})(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*])"
+    );
+
+    if (regex.test(password)) {
+      const user = {
+        name: name,
+        email: email,
+        password: password,
+      };
+      setUserToRegister(user);
+      register(user);
+      setFeedbackMessage("Du har nu registrerat dig!");
+    } else {
+      setFeedbackMessage(
+        "Ditt lösenord måste vara minst 6 tecken långt, innehålla både stora och små bokstäver samt ett specialtecken."
+      );
+    }
+  };
 
   return (
     <div>
