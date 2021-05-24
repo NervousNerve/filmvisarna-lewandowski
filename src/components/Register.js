173dcbd37a499594dba055dafcce351e27f3362b
Saga <sagaswahn@hotmail.com>
2021-05-24 13:16:04 +0200

    Created register component

---
+++ src/components/Register.js
@@ -0,0 +1,29 @@
+import styles from "./css/Register.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLock, faUser } from "@fortawesome/free-solid-svg-icons";
+
+const Register = () => {
+  return (
+    <div>
+      <h1>Sign up</h1>
+      <p>You have to log in to book tickets.</p>
+      <form>
+        <div>
+          <FontAwesomeIcon icon={faUser} />
+          <input placeholder="name" type="email" />
+        </div>
+        <div>
+          <FontAwesomeIcon icon={faUser} />
+          <input placeholder="email" type="text" />
+        </div>
+        <div>
+          <FontAwesomeIcon icon={faLock} />
+          <input placeholder="password" type="text" />
+        </div>
+        <button>Create account</button>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
