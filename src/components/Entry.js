697f3e53557c7f959c6a1a5cca5689186e0e8e81
Saga <sagaswahn@hotmail.com>
2021-05-24 13:19:53 +0200

    Created entry component

---
+++ src/components/Entry.js
@@ -0,0 +1,10 @@
+import Login from "./components/Login";
+import Register from "./components/Register";
+import { useState } from "react";
+import styles from "./css/Entry.module.css";
+
+const Entry = () => {
+  return <div></div>;
+};
+
+export default Entry;
