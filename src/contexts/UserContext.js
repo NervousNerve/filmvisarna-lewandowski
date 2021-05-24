8e726a697e8de4fc192e879dfc1b6faa5e340fe8
Saga <sagaswahn@hotmail.com>
2021-05-24 10:11:30 +0200

    Fetch whoamI on render and set currentUser

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 export const UserContext = createContext();
 
@@ -6,23 +6,41 @@ const UserProvider = (props) => {
   const [currentUser, setCurrentUser] = useState(undefined);
 
   useEffect(() => {
-    whoami();
+    // TODO: remove this function when we have a functioning login interface
+    // explanation: it is a function to automatically log a user in on render
+    (async () => {
+      if (!(await whoami())) {
+        let loggedInUser = await fetch("/api/v1/users/login", {
+          method: "POST",
+          headers: { "content-type": "application/json" },
+          body: JSON.stringify({
+            email: "test@testing4",
+            password: "1234",
+          }),
+        });
+        loggedInUser = await loggedInUser.json();
+        setCurrentUser(loggedInUser);
+      }
+    })();
   }, []);
 
-  const whoami = async () => {
-    let user = await fetch("/api/v1/users/whoami", {
-      method: "POST",
-      headers: { "content-type": "application/json" },
-      body: JSON.stringify(),
-    });
-    user = await user.json();
+  // TODO: delete when testing is done
+  useEffect(() => {
+    console.log(currentUser);
+  }, [currentUser]);
 
-    if (user.success) {
-      setCurrentUser(user);
-      console.log(currentUser);
-    } else if (user.error) {
-      console.log(user.error);
+  const whoami = async () => {
+    let user = await fetch("/api/v1/users/whoami");
+    user = await user.json();
+    setCurrentUser(user);
+
+    if (user) {
+      console.log("Got a user");
+    } else {
+      console.log(user);
     }
+
+    return user;
   };
 
   const values = { currentUser };
