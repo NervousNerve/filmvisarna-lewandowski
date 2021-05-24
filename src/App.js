223b1358acfce65ef6e671f4e77a3afba70d89c2
Amanda <amandaelizaolsson@gmail.com>
2021-05-24 13:35:01 +0200

    Created profile page and added route in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import UserProvider from "./contexts/UserContext";
+import ProfilePage from "./pages/ProfilePage";
 
 function App() {
   return (
     <div className="App">
       <UserProvider>
-        <BrowserRouter></BrowserRouter>
+        <BrowserRouter>
+          <Route exact path="/profile" component={ProfilePage} />
+        </BrowserRouter>
       </UserProvider>
     </div>
   );
