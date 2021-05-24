4bf1c38319e940f514738adb214bc1fb6d9eabff
Saga <sagaswahn@hotmail.com>
2021-05-24 19:23:52 +0200

    Created state variables for register

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,4 +1,10 @@
+import { useState } from "react";
+
 const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div>
       <h1>Sign up</h1>
