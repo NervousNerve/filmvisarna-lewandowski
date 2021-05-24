cff006a9fc72d7f89a037488507f25fcecce4eea
Saga <sagaswahn@hotmail.com>
2021-05-24 19:18:50 +0200

    Finalised login-function

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -5,30 +5,11 @@ export const UserContext = createContext();
 const UserProvider = (props) => {
   const [currentUser, setCurrentUser] = useState(undefined);
   const [userToLogin, setUserToLogin] = useState([]);
+  const [feedbackMessage, setFeedbackMessage] = useState(null);
 
   useEffect(() => {
-    // TODO: remove this function when we have a functioning login interface
-    // explanation: it is a function to automatically log a user in on render
-    (async () => {
-      if (!(await whoami())) {
-        let loggedInUser = await fetch("/api/v1/users/login", {
-          method: "POST",
-          headers: { "content-type": "application/json" },
-          body: JSON.stringify({
-            email: "test@testing4",
-            password: "1234",
-          }),
-        });
-        loggedInUser = await loggedInUser.json();
-        setCurrentUser(loggedInUser);
-      }
-    })();
+    whoami();
   }, []);
-
-  // TODO: delete when testing is done
-  useEffect(() => {
-    console.log(currentUser);
-  }, [currentUser]);
 
   const whoami = async () => {
     let user = await fetch("/api/v1/users/whoami");
@@ -45,22 +26,27 @@ const UserProvider = (props) => {
   };
 
   const login = async (userToLogin) => {
-    console.log(userToLogin);
     let user = await fetch("/api/v1/users/login", {
       method: "POST",
       headers: { "content-type": "application/json" },
       body: JSON.stringify(userToLogin),
     });
     user = await user.json();
+    console.log(user);
 
     if (user.success) {
       setCurrentUser(user);
+      // push to profile page
     } else if (user.error) {
+      setFeedbackMessage("Email or password is invalid");
+      setTimeout(() => {
+        setFeedbackMessage(null);
+      }, 3000);
       console.log(user.error);
     }
   };
 
-  const values = { currentUser, login, setUserToLogin };
+  const values = { currentUser, login, setUserToLogin, feedbackMessage };
 
   return (
     <UserContext.Provider value={values}>{props.children}</UserContext.Provider>
