f01e6437264ab13db1bc69ccd99c8ac9ff383f63
Amanda <amandaelizaolsson@gmail.com>
2021-05-25 10:28:24 +0200

    Updating route for getting user bookings
    Amanda & Henrik.

--- api/routes/bookingRoutes.js
+++ api/routes/bookingRoutes.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 const bookingController = require("../controllers/bookingController");
 
-router.get("/userbookings", bookingController.getBookingsByUser);
 router.get("/:id", bookingController.getBookingById);
+router.get("/", bookingController.getBookingsByUser);
 router.post("/", bookingController.createBooking);
 
 module.exports = router;
