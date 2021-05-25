4a70e31cd1ed2bcebd87cac0dacf29069e8b7cba
Stephanie <puchastephanie@gmail.com>
2021-05-25 12:09:04 +0200

    clean navbar.js

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,5 @@
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
-// import { Link } from "react-router-dom";
-// import { slide as Menu } from 'react-burger-menu'
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
@@ -18,28 +16,8 @@ const handleClick = () => {
   }
 };
 
-//       <faSearch>
-//         <a id="home" className="menu-item" href="/">Home</a>
-//         <a id="about" className="menu-item" href="/about">About</a>
-//         <a id="contact" className="menu-item" href="/contact">Contact</a>
-//         <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
-//       </faSearch>
-//     );
-//   }
-// }
-
 const Navbar = () => {
   //   // eslint-disable-next-line
-  //   const [links, setLinks] = useState([{ name: "Channels", url: "/" }, { name: "Programs category", url: "/programcategory" }, { name: "Favorite", url: "/favorite" }, {name: "Register/Login", url:"/login"}]);
-
-  //   const renderLinks = () => {
-  //     return links.map((link) => (
-  //       <Link className={styles.link} key={link.name} to={link.url}>
-  //         {link.name}
-  //       </Link>
-  //     ));
-  //   };
-  // function Navbar(props) {
   const [filmLogo, setFilmLogo] = useState(
     "https://trello-attachments.s3.amazonaws.com/60a21d927cb7b38110c05826/60ab5f1eb07e002ab9bbcfb5/3429140ee35b63a2ad4a06612bacce6f/logo-1.png"
   );
@@ -61,20 +39,8 @@ const Navbar = () => {
       </div>
         <FontAwesomeIcon className={styles.searchicon2} icon={faSearch} />
       </div>
-      {/* <div className={styles.topnav} id="myMenu">
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/my-profile">My profile</NavLink>
-        <NavLink to="/login-register">Login/Register</NavLink>
-      </div> */}
-
-      {/* <img className={styles.img} src={marvelLogo} alt="Logo" /> */}
     </div>
   );
-
-  // }
-  //   return <nav >
-  //     <FontAwesomeIcon icon={faBars}/>
-  //     <FontAwesomeIcon icon={faSearch}/></nav>;
 };
 
 export default Navbar;
