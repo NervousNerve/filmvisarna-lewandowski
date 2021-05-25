1770d97bfe2afdc315961cf0e0b837084a5b0a82
Amanda <amandaelizaolsson@gmail.com>
2021-05-25 10:08:10 +0200

    Added comment

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,6 +1,8 @@
 import { useContext, useEffect, useState } from "react";
 import { UserContext } from "../contexts/UserContext";
 
+//ability to edit user info to be implemented in next sprint
+
 const ProfilePage = () => {
   const { currentUser } = useContext(UserContext);
   const [displayUpcoming, setDisplayUpcoming] = useState(true);
