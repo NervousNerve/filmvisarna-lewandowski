eb9adacada09dfe5f3b24df2483759146b4d1cd2
Saga <sagaswahn@hotmail.com>
2021-05-25 11:39:25 +0200

    Created CSS module for Booking

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter } from "react-router-dom";
 import UserProvider from "./contexts/UserContext";
+import Booking from "./components/Booking";
 
 function App() {
   return (
     <div className="App">
       <UserProvider>
+        <Booking />
         <BrowserRouter></BrowserRouter>
       </UserProvider>
     </div>
--- src/components/Booking.js
+++ src/components/Booking.js
@@ -1,5 +1,16 @@
+import styles from "../css/Booking.module.css";
+
 const Booking = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className={styles.pricetypeWrapper}>
+        <p>Adult</p>
+        <span>-</span>
+        <div>1</div>
+        <span>+</span>
+      </div>
+    </div>
+  );
 };
 
 export default Booking;
