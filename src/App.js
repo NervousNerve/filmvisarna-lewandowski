286a6032376b8c26c1772b0798febdb57f9a05eb
Stephanie <puchastephanie@gmail.com>
2021-05-25 13:43:54 +0200

    Added confirmation route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import Navbar from "./components/Navbar.jsx";
+import ConfirmationPage from "./pages/ConfirmationPage";
 
 function App() {
   return (
     <div className="App">
-      
       <BrowserRouter>
-      <Navbar/>
+        <Navbar />
+        <Route exact path="/confirmation" component={ConfirmationPage} />
       </BrowserRouter>
     </div>
   );
