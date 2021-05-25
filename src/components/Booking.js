69641b224c2717822c9f1a2e0f80f146575e38e2
Saga <sagaswahn@hotmail.com>
2021-05-25 15:28:27 +0200

    Added NumberInput component

--- src/components/Booking.js
+++ src/components/Booking.js
@@ -1,58 +1,29 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus, faMinus } from "@fortawesome/free-solid-svg-icons";
 import styles from "../css/Booking.module.css";
+import NumberInput from "./NumberInput";
 
 const Booking = () => {
   return (
     <div className={styles.bookingWrapper}>
       <div className={styles.pricetypeWrapper}>
         <p>Adult</p>
-        <div className={styles.addRemoveWrapper}>
-          <div className={styles.addRemove}>
-            <button>
-              <FontAwesomeIcon icon={faMinus} />
-            </button>
-            <span>1</span>
-            <button>
-              <FontAwesomeIcon icon={faPlus} />
-            </button>
-          </div>
-        </div>
+        <NumberInput />
       </div>
       <div className={styles.pricetypeWrapper}>
         <p>Child</p>
-        <div className={styles.addRemoveWrapper}>
-          <div className={styles.addRemove}>
-            <button>
-              <FontAwesomeIcon icon={faMinus} />
-            </button>
-            <span>1</span>
-            <button>
-              <FontAwesomeIcon icon={faPlus} />
-            </button>
-          </div>
-        </div>
+        <NumberInput />
       </div>
       <div className={styles.pricetypeWrapper}>
         <p>Oldie</p>
-        <div className={styles.addRemoveWrapper}>
-          <div className={styles.addRemove}>
-            <button>
-              <FontAwesomeIcon icon={faMinus} />
-            </button>
-            <span>1</span>
-            <button>
-              <FontAwesomeIcon icon={faPlus} />
-            </button>
-          </div>
-        </div>
+        <NumberInput />
       </div>
 
       <div className={styles.selectWrapper}>
-        <select className="custom-select">
-          <option>test</option>
-          <option>test</option>
-        </select>
+        <div className="custom-select">
+          <select>
+            <option>test</option>
+            <option>test</option>
+          </select>
+        </div>
       </div>
     </div>
   );
