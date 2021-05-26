6a9f92abe378ceddd56a362a82bc4861d40ff374
Stephanie <puchastephanie@gmail.com>
2021-05-26 10:40:43 +0200

    Done fixing navbar responsitivity

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,25 +7,44 @@ import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 import styles from "../css/Navbar.module.css";
 
-const handleClick = () => {
-  var menu = document.getElementById("myMenu");
-  if (menu.style.display === "block") {
-    menu.style.display = "none";
-  } else {
-    menu.style.display = "block";
-  }
-};
+// const handleClick = () => {
+//   var menu = document.getElementById("myMenu");
+//   if (menu.style.display === "block") {
+//     menu.style.display = "none";
+//   } else {
+//     menu.style.display = "block";
+//   }
+// };
 
 const Navbar = () => {
-  //   // eslint-disable-next-line
-  const [filmLogo, setFilmLogo] = useState(
-    "https://trello-attachments.s3.amazonaws.com/60a21d927cb7b38110c05826/60ab5f1eb07e002ab9bbcfb5/3429140ee35b63a2ad4a06612bacce6f/logo-1.png"
-  );
+  // eslint-disable-next-line
+  const filmLogo =
+    "https://trello-attachments.s3.amazonaws.com/60a21d927cb7b38110c05826/60ab5f1eb07e002ab9bbcfb5/3429140ee35b63a2ad4a06612bacce6f/logo-1.png";
+  
+    const [menu, setMenu] = useState(false);
+
+  const handleClick = () => {
+    if (menu === false) {
+      setMenu(true);
+    } else {
+      setMenu(false);
+    }
+  };
+
+  // const handleClick = () => {
+  //   var menu = document.getElementById("myMenu");
+  //   if (menu.style.display === "block") {
+  //     menu.style.display = "none";
+  //   } else {
+  //     menu.style.display = "block";
+  //   }
+  // };
 
   return (
     <div>
-      <div className={styles.navs}>
+      <div className={`${styles.navs} ${menu && styles.clickedMenu}`}>
         <FontAwesomeIcon
+          // className={`${styles.burger} ${menu && styles.clickedMenu}`}
           className={styles.burger}
           icon={faBars}
           onClick={handleClick}
@@ -33,10 +52,18 @@ const Navbar = () => {
         <img className={styles.img} src={filmLogo} alt="Logo" />
         <FontAwesomeIcon className={styles.searchicon} icon={faSearch} />
         <div className={styles.topnav} id="myMenu">
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/my-profile">My profile</NavLink>
-          <NavLink to="/confirmation">Confirmation</NavLink>
-          <NavLink to="/login-register">Login/Register</NavLink>
+          <NavLink onClick={handleClick} to="/">
+            Home
+          </NavLink>
+          <NavLink onClick={handleClick} to="/my-profile">
+            My profile
+          </NavLink>
+          <NavLink onClick={handleClick} to="/confirmation">
+            Confirmation
+          </NavLink>
+          <NavLink onClick={handleClick} to="/login-register">
+            Login/Register
+          </NavLink>
         </div>
 
         <div className={styles.field}>
