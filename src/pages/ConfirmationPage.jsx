66be82445150e4dc562998680717f44f1794318b
Stephanie <puchastephanie@gmail.com>
2021-05-31 11:29:32 +0200

    Edited css

--- src/pages/ConfirmationPage.jsx
+++ src/pages/ConfirmationPage.jsx
@@ -26,43 +26,45 @@ const ConfirmationPage = (props) => {
   }
 
   return (
-    <div>
-      <div className={styles.confirmation}>
-        <img
-          className={styles.confirm}
-          src="/assets/icons/confirm.svg"
-          alt="confirm icon"
-        />
-        <div className={styles.thanks}>
-          <h1>Thank you for booking with us!</h1>
-          <p>Your reservation is reserved.</p>
-        </div>
-        {/* <hr /> */}
-        <div className={styles.details}>
-          <div className={styles.orderDetails}>
-            <div className={styles.detailsLeft}>
-              <p>Booking Number:</p>
-              <p>Price:</p>
-              <p>Date/Time:</p>
-              <p>Seat/Row:</p>
-              <p>Film description:</p>
-            </div>
-            <div className={styles.detailsRight}>
-              <p> {booking._id}</p>
-              <p> {booking.price} kr</p>
-              <p> {booking.screeningId.date}</p>
-              <p> {booking.seats.join(", ")}</p>
-              <p> {booking.screeningId.movieId.title}</p>
-              <p className={styles.plot}> {booking.screeningId.movieId.plot}</p>
-            </div>
-          </div>
-        </div>
-        <div className={styles.printButton}>
-          <button onClick={handleClick} className={styles.backhome}>
-            Back to Home
-          </button>
-        </div>
-        <div></div>
+    <div className={styles.confirmation}>
+      <img
+        className={styles.confirm}
+        src="/assets/icons/confirm.svg"
+        alt="confirm icon"
+      />
+      <div className={styles.thanks}>
+        <h1>Thank you for booking with us!</h1>
+        <h2>Your reservation is confirmed.</h2>
+      </div>
+      {/* <hr /> */}
+      {/* <div className={styles.details}> */}
+      <div className={styles.orderDetails}>
+        {/* <div className={`${styles.detailsLeft}`}> */}
+        <h4>Booking Number:</h4>
+        <p className={styles.p1}> {booking._id}</p>
+        <h4>Price:</h4>
+        <p> {booking.price} kr</p>
+        <h4>Date/Time:</h4>
+        <p> {booking.screeningId.date}</p>
+        <h4>Seat/Row:</h4>
+        <p> {booking.seats.join(", ")}</p>
+        <h4>Film description: </h4>
+        <p className={styles.plot}> {booking.screeningId.movieId.plot}</p>
+        {/* </div> */}
+        {/* <div className={styles.detailsRight}>
+            <p> {booking._id}</p>
+            <p> {booking.price} kr</p>
+            <p> {booking.screeningId.date}</p>
+            <p> {booking.seats.join(", ")}</p>
+            <p> {booking.screeningId.movieId.title}</p>
+            <p className={styles.plot}> {booking.screeningId.movieId.plot}</p>
+          </div> */}
+      </div>
+      {/* </div> */}
+      <div className={styles.printButton}>
+        <button onClick={handleClick} className={styles.backhome}>
+          Back to Home
+        </button>
       </div>
     </div>
   );
