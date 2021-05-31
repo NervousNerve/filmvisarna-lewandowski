7137720f5e22091750d02805eede49b31a2d9477
Lisa Hansson <lisastina.hansson@gmail.com>
2021-05-31 15:55:31 +0200

    Logo onClick push to home

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useHistory } from "react-router-dom";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
@@ -8,9 +8,7 @@ import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import styles from "../css/Navbar.module.css";
 
 const Navbar = () => {
-  // eslint-disable-next-line
-  // const filmLogo =
-  //   "https://trello-attachments.s3.amazonaws.com/60a21d927cb7b38110c05826/60ab5f1eb07e002ab9bbcfb5/3429140ee35b63a2ad4a06612bacce6f/logo-1.png";
+  const history = useHistory();
 
   const [menu, setMenu] = useState(false);
 
@@ -36,7 +34,12 @@ const Navbar = () => {
             />
           </div>
 
-          <img className={styles.img} src="/assets/icons/logo.png" alt="Logo" />
+          <img
+            className={styles.img}
+            src="/assets/icons/logo.png"
+            alt="Logo"
+            onClick={() => history.push("/")}
+          />
 
           <div
             className={`${styles.grid} ${styles.justifyEnd} ${styles.alignCenter}`}
