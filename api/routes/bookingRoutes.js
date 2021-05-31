686563e987b543f0730b733ca1b25aed54d6f080
Saga <sagaswahn@hotmail.com>
2021-05-31 10:34:09 +0200

    Created route for getAllRebates

--- api/routes/bookingRoutes.js
+++ api/routes/bookingRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const bookingController = require("../controllers/bookingController");
 
+router.get("/rebates", bookingController.getAllRebates);
 router.get("/:id", bookingController.getBookingById);
 router.get("/", bookingController.getBookingsByUser);
 router.post("/", bookingController.createBooking);
