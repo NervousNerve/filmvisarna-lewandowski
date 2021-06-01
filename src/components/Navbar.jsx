6ee5765e218cf5e7b8b357813f0d209296f3f0e3
Saga <sagaswahn@hotmail.com>
2021-06-01 11:40:13 +0200

    Removed warning about unused import

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,6 @@ import { NavLink, useHistory } from "react-router-dom";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 import styles from "../css/Navbar.module.css";
 
