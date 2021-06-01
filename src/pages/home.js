30f334705d623ec0822bacb6606c6155a4e03fa8
Saga <sagaswahn@hotmail.com>
2021-06-01 18:25:12 +0200

    Positioning text on top of image

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,13 +4,11 @@ import styles from "../css/Home.module.css";
 const Home = () => {
   return (
     <div className={styles.pageWrapper}>
-      <div className={styles.imgContainer}>
-        {/* <img
-          src="https://m.media-amazon.com/images/M/MV5BNDExNDYxODc1MF5BMl5BanBnXkFtZTcwNDQ0OTk1Mw@@._V1_.jpg"
-          alt="movie-img"
-        ></img> */}
+      <div className={styles.imgContainer}></div>
+      <div className={styles.textContainer}>
+        <h1>In theatres now</h1>
+        <p>Inception</p>
       </div>
-      <h1>In theatres now!</h1>
       <h3>Popular shows</h3>
       <MovieList />
     </div>
