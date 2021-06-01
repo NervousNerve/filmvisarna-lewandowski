1e5a7fa9b428bb6c834ae6709d9b2812d6e26b99
Saga <sagaswahn@hotmail.com>
2021-06-01 15:18:57 +0200

    Added headline

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,6 +3,7 @@ import MovieList from "../components/movieList";
 const Home = () => {
   return (
     <div className="home">
+      <h1>In theatres right now</h1>
       <MovieList />
     </div>
   );
