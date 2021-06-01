23eaf4ac24a88286f2a83c3a2e849b6b9caa7815
Saga <sagaswahn@hotmail.com>
2021-06-01 18:32:43 +0200

    Styling text on image

--- src/pages/home.js
+++ src/pages/home.js
@@ -6,8 +6,8 @@ const Home = () => {
     <div className={styles.pageWrapper}>
       <div className={styles.imgContainer}></div>
       <div className={styles.textContainer}>
-        <h1>In theatres now</h1>
-        <p>Inception</p>
+        <h4>In theatres now</h4>
+        <h2>Inception</h2>
       </div>
       <h3>Popular shows</h3>
       <MovieList />
