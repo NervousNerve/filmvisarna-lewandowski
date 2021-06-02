2cbbd10a5b9adf786a0efd040a949c710117add4
Stephanie <puchastephanie@gmail.com>
2021-06-02 13:36:20 +0200

    Trying to put feedback message on login

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -5,6 +5,7 @@ export const UserContext = createContext();
 const UserProvider = (props) => {
   const [currentUser, setCurrentUser] = useState(undefined);
   const [feedbackMessage, setFeedbackMessage] = useState(null);
+  const [feedbackMessageOk, setFeedbackMessageOk] = useState(null);
 
   useEffect(() => {
     whoami();
@@ -31,6 +32,8 @@ const UserProvider = (props) => {
       }, 3000);
       console.log(user.error);
       return;
+    } else {
+      setFeedbackMessageOk("You are successfully log in");
     }
 
     setCurrentUser(user);
@@ -59,6 +62,7 @@ const UserProvider = (props) => {
   const values = {
     currentUser,
     feedbackMessage,
+    feedbackMessageOk,
     login,
     register,
   };
