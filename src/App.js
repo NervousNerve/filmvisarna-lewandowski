14ea1d830e79fb9e9d34a89b88915ab11dd59c82
Saga <sagaswahn@hotmail.com>
2021-06-02 09:53:10 +0200

    Removed unused imports

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { BrowserRouter, Route } from "react-router-dom";
 import Navbar from "./components/Navbar.jsx";
 import ConfirmationPage from "./pages/ConfirmationPage";
-import UserProvider from "./contexts/UserContext";
 import Home from "./pages/home";
 import MoviePage from "./pages/MoviePage";
 import GuardedRoute from "./components/GuardedRoute";
@@ -16,16 +15,21 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-          <QueryParamProvider ReactRouterRoute={Route}>
-            <Navbar />
-            <Route exact path="/">
-              <Home />
-            </Route>
-        <Route exact path="/confirmation/:id" component={ConfirmationPage} />
-        <Route exact path="/movies/:movieId" component={MoviePage} />
-        <GuardedRoute exact path="/profile" component={ProfilePage} auth={currentUser} />
-          </QueryParamProvider>
-        </BrowserRouter>
+        <QueryParamProvider ReactRouterRoute={Route}>
+          <Navbar />
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/confirmation/:id" component={ConfirmationPage} />
+          <Route exact path="/movies/:movieId" component={MoviePage} />
+          <GuardedRoute
+            exact
+            path="/profile"
+            component={ProfilePage}
+            auth={currentUser}
+          />
+        </QueryParamProvider>
+      </BrowserRouter>
     </div>
   );
 }
