326880d43f89d4b0c4628cc4cf4c4580342a1f3c
Johan Markström <johan.markstrom@gmail.com>
2021-06-03 12:16:06 +0200

    changed movieCard to Link element
    changed basic styling

--- src/components/movieCard.js
+++ src/components/movieCard.js
@@ -1,40 +1,21 @@
 import styles from "../css/Movie-card.module.css";
-import { useHistory } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const MovieCard = (props) => {
-  const history = useHistory();
-
-  const handleMovieClick = (movidId, e) => {
-    history.push(`/movies/${movidId}`);
-  };
-
-  //checks if user pressed enter
-  const handleKeyPress = (e) => {
-    if (e.keyCode === 13) {
-      handleMovieClick(props.movie._id);
-    }
-  };
-
   return (
-    <div
+    <Link
       className={styles.movieCard}
-      onClick={() => {
-        handleMovieClick(props.movie._id);
-      }}
-      onKeyDown={handleKeyPress}
-      tabIndex="0"
+      style={{ textDecoration: "none" }}
+      to={`/movies/${props.movie._id}`}
     >
-      <div className={styles.imageWrapper}>
-        <img
-          className={styles.movieImage}
-          src={props.movie.imageUrl}
-          alt="movie thumbnail"
-        />
-      </div>
+      <div
+        className={styles.imageWrapper}
+        style={{ backgroundImage: `url("${props.movie.imageUrl}")` }}
+      ></div>
       <div className={styles.titleWrapper}>
         <h4 className={styles.movieTitle}>{props.movie.title}</h4>
       </div>
-    </div>
+    </Link>
   );
 };
 
