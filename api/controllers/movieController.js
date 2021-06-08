973aee099f7825aa848755cf48e94244b1db8d8c
Johan Markström <johan.markstrom@gmail.com>
2021-06-08 16:18:33 +0200

    Added if-check
    Henrik Lood, Johan Markström

--- api/controllers/movieController.js
+++ api/controllers/movieController.js
@@ -6,40 +6,45 @@ const getAllMovies = async (req, res) => {
   // let option = `$options: "i"`;
 
   try {
-    let movies = await Movie.find({
-      $or: [
-        { title: { $regex: searchQuery || "", $options: "i" } },
-        { plot: { $regex: searchQuery || "", $options: "i" } },
-        { fullPlot: { $regex: searchQuery || "", $options: "i" } },
-        { director: { $regex: searchQuery || "", $options: "i" } },
-        {
-          actors: {
-            $elemMatch: { $regex: searchQuery || "", $options: "i" },
+    if (req.query) {
+      let movies = await Movie.find({
+        $or: [
+          { title: { $regex: searchQuery || "", $options: "i" } },
+          { plot: { $regex: searchQuery || "", $options: "i" } },
+          { fullPlot: { $regex: searchQuery || "", $options: "i" } },
+          { director: { $regex: searchQuery || "", $options: "i" } },
+          {
+            actors: {
+              $elemMatch: { $regex: searchQuery || "", $options: "i" },
+            },
           },
-        },
-      ],
+        ],
 
-      genre: {
-        $elemMatch: { $regex: req.query.genre || "", $options: "i" },
-      },
-      language: {
-        $elemMatch: { $regex: req.query.language || "", $options: "i" },
-      },
-      actors: {
-        $elemMatch: { $regex: req.query.actors || "", $options: "i" },
-      },
-      director: { $regex: req.query.director || "", $options: "i" },
-      runtime: {
-        $gte: req.query.minRuntime || 0,
-        $lte: req.query.maxRuntime || 1000,
-      },
-      rated: { $regex: req.query.rated || "", $options: "i" },
-      price: {
-        $gte: req.query.minPrice || 0,
-        $lte: req.query.maxPrice || 1000,
-      },
-    }).exec();
-    res.json(movies);
+        genre: {
+          $elemMatch: { $regex: req.query.genre || "", $options: "i" },
+        },
+        language: {
+          $elemMatch: { $regex: req.query.language || "", $options: "i" },
+        },
+        actors: {
+          $elemMatch: { $regex: req.query.actors || "", $options: "i" },
+        },
+        director: { $regex: req.query.director || "", $options: "i" },
+        runtime: {
+          $gte: req.query.minRuntime || 0,
+          $lte: req.query.maxRuntime || 1000,
+        },
+        rated: { $regex: req.query.rated || "", $options: "i" },
+        price: {
+          $gte: req.query.minPrice || 0,
+          $lte: req.query.maxPrice || 1000,
+        },
+      }).exec();
+      res.json(movies);
+    } else {
+      let movies = await Movie.find().exec();
+      res.json(movies);
+    }
   } catch (err) {
     console.log(err);
     res.status(500).json({ error: "Could not read filter parameters" });
