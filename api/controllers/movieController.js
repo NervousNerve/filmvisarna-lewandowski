3d322d8c352ba3dd8d189795d83df4a30aa4f33e
Johan Markström <johan.markstrom@gmail.com>
2021-06-09 15:32:10 +0200

    Added filter for Date
    Henrik Lood, Johan Markström, Sebastian Mineur

--- api/controllers/movieController.js
+++ api/controllers/movieController.js
@@ -1,50 +1,63 @@
 const Movie = require("../models/Movie");
 
 const getAllMovies = async (req, res) => {
-  let searchQuery = `${req.query.search}`;
-
-  // let option = `$options: "i"`;
+  let searchQuery = req.query.search || "";
+  let minDate = new Date(req.query.date || "1000-01-01");
+  let maxDate = new Date(req.query.date || "3000-01-01");
+  console.log("These are dates: ", minDate, maxDate);
 
   try {
-    if (req.query) {
-      let movies = await Movie.find({
-        $or: [
-          { title: { $regex: searchQuery || "", $options: "i" } },
-          { plot: { $regex: searchQuery || "", $options: "i" } },
-          { fullPlot: { $regex: searchQuery || "", $options: "i" } },
-          { director: { $regex: searchQuery || "", $options: "i" } },
-          {
-            actors: {
-              $elemMatch: { $regex: searchQuery || "", $options: "i" },
-            },
-          },
-        ],
+    let movies = await Movie.aggregate([
+      {
+        $lookup: {
+          from: "screenings",
+          localField: "_id",
+          foreignField: "movieId",
+          as: "screenings",
+        },
+      },
+      {
+        $match: {
+          "screenings.date": { $lte: maxDate, $gte: minDate },
+        },
+      },
 
-        genre: {
-          $elemMatch: { $regex: req.query.genre || "", $options: "i" },
+      {
+        $match: {
+          $or: [
+            { title: { $regex: searchQuery, $options: "i" } },
+            { plot: { $regex: searchQuery, $options: "i" } },
+            { fullPlot: { $regex: searchQuery, $options: "i" } },
+            { director: { $regex: searchQuery, $options: "i" } },
+            {
+              actors: {
+                $elemMatch: { $regex: searchQuery, $options: "i" },
+              },
+            },
+          ],
+          genre: {
+            $elemMatch: { $regex: req.query.genre || "", $options: "i" },
+          },
+          language: {
+            $elemMatch: { $regex: req.query.language || "", $options: "i" },
+          },
+          actors: {
+            $elemMatch: { $regex: req.query.actors || "", $options: "i" },
+          },
+          director: { $regex: req.query.director || "", $options: "i" },
+          runtime: {
+            $gte: req.query.minRuntime || 0,
+            $lte: req.query.maxRuntime || 1000,
+          },
+          rated: { $regex: req.query.rated || "", $options: "i" },
+          price: {
+            $gte: req.query.minPrice || 0,
+            $lte: req.query.maxPrice || 1000,
+          },
         },
-        language: {
-          $elemMatch: { $regex: req.query.language || "", $options: "i" },
-        },
-        actors: {
-          $elemMatch: { $regex: req.query.actors || "", $options: "i" },
-        },
-        director: { $regex: req.query.director || "", $options: "i" },
-        runtime: {
-          $gte: req.query.minRuntime || 0,
-          $lte: req.query.maxRuntime || 1000,
-        },
-        rated: { $regex: req.query.rated || "", $options: "i" },
-        price: {
-          $gte: req.query.minPrice || 0,
-          $lte: req.query.maxPrice || 1000,
-        },
-      }).exec();
-      res.json(movies);
-    } else {
-      let movies = await Movie.find().exec();
-      res.json(movies);
-    }
+      },
+    ]).exec();
+    res.json(movies);
   } catch (err) {
     console.log(err);
     res.status(500).json({ error: "Could not read filter parameters" });
