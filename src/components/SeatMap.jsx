cf7f32104a1396349d709df1af8e1abe8e64bb7a
Lisa Hansson <lisastina.hansson@gmail.com>
2021-06-09 15:16:41 +0200

    Elements for description of seats

--- src/components/SeatMap.jsx
+++ src/components/SeatMap.jsx
@@ -4,4 +4,25 @@ const SeatMap = () => {
   return <div className={style.seatMap}></div>;
 };
 
+  return (
+    <div className={style.seatMap}>
+      <div className={style.seatDescWrapper}>
+        <div className={style.seatDescContainer}>
+          <div className={`${style.seatDesc} ${style.booked}`}></div>
+          <p>Booked</p>
+        </div>
+        <div className={style.seatDescContainer}>
+          <div className={`${style.seatDesc} ${style.available}`}></div>
+          <p>Available</p>
+        </div>
+        <div className={style.seatDescContainer}>
+          <div className={`${style.seatDesc} ${style.yours}`}></div>
+          <p>Your seats</p>
+        </div>
+      </div>
+      <hr />
+    </div>
+  );
+};
+
 export default SeatMap;
