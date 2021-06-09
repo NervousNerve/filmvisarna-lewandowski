427e28f4cf24f128b24b63bbfdd6ddaae93f61b4
Lisa Hansson <lisastina.hansson@gmail.com>
2021-06-09 15:25:53 +0200

    Temporary screening and fetch of theater to get seats

--- src/components/SeatMap.jsx
+++ src/components/SeatMap.jsx
@@ -1,8 +1,29 @@
 import style from "../css/SeatMap.module.css";
+import { useState, useEffect } from "react";
 
 const SeatMap = () => {
-  return <div className={style.seatMap}></div>;
-};
+  const [theater, setTheater] = useState();
+
+  const screening = {
+    _id: "60a655dead5bec403ce90cef",
+    occupiedseats: [
+      1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+    ],
+    date: "2021-06-06T12:00:00.000+00:00",
+    movieId: "60a632b98421e91fe4243b9e",
+    theaterId: "60a6435192b1562b546900d6",
+  };
+
+  useEffect(() => {
+    (async () => {
+      let theaters = await fetch(`/api/v1/theaters`);
+      theaters = await theaters.json();
+      const theater = theaters.find(
+        (theater) => theater._id === screening.theaterId
+      );
+      setTheater(theater);
+    })();
+  }, []);
 
   return (
     <div className={style.seatMap}>
