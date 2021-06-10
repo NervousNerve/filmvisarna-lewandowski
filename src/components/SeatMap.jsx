d16c3db919495a2c6d1c9e3f4934c44139bb1185
Lisa Hansson <lisastina.hansson@gmail.com>
2021-06-10 16:21:37 +0200

    Clean up and comments

--- src/components/SeatMap.jsx
+++ src/components/SeatMap.jsx
@@ -1,21 +1,20 @@
 import style from "../css/SeatMap.module.css";
 import { useState, useEffect } from "react";
 
-const SeatMap = () => {
+const SeatMap = (props) => {
   const [theater, setTheater] = useState(null);
 
+  /* remove and get from booking props */
   const [selectedSeats, setSelectedSeats] = useState([]);
   const tickets = 3;
   const screening = {
-    _id: "60a655dead5bec403ce90cef",
     occupiedSeats: [
       1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
     ],
-    date: "2021-06-06T12:00:00.000+00:00",
-    movieId: "60a632b98421e91fe4243b9e",
     theaterId: "60a6435192b1562b546900d5",
   };
 
+  /* fetch theaters and find right theater */
   useEffect(() => {
     (async () => {
       let theaters = await fetch(`/api/v1/theaters`);
@@ -28,6 +27,7 @@ const SeatMap = () => {
     })();
   }, []);
 
+  /* set selected seats state */
   const handleChange = (e) => {
     if (selectedSeats.includes(parseInt(e.target.value))) {
       setSelectedSeats(
@@ -35,13 +35,15 @@ const SeatMap = () => {
       );
       return;
     }
+    /* prevent selection of more seats than tickets */
     if (selectedSeats.length >= tickets) {
       e.preventDefault();
       return;
     }
-    setSelectedSeats((prevState) => [...prevState, parseInt(e.target.value)]);
+    setSelectedSeats([...selectedSeats, parseInt(e.target.value)]);
   };
 
+  /* generate seats with seat numbers */
   const generateSeats = () => {
     let offset = 0;
     let rows = [];
@@ -57,6 +59,7 @@ const SeatMap = () => {
               className={style.seat}
               value={seatNumber}
               onChange={handleChange}
+              /* if seat is occupied, make it disabled */
               disabled={screening.occupiedSeats.includes(seatNumber)}
             />
             <div className={style.checkmark}></div>
@@ -81,6 +84,7 @@ const SeatMap = () => {
             theater.name === "Royal" && style.largeTheater
           }`}
         >
+          {/* Simplify seatDescs?? */}
           <div className={style.seatDescWrapper}>
             <div className={style.seatDescContainer}>
               <div className={`${style.seatDesc} ${style.booked}`}></div>
