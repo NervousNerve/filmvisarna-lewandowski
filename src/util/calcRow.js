cc385bd11ef147f445d659d4fd2ce9cfcd27de98
Sebastian Mineur <sebastian.mineur@gmail.com>
2021-06-11 16:10:50 +0200

    Refactor util function to just return row number

--- src/util/calcRow.js
+++ src/util/calcRow.js
@@ -1,11 +1,11 @@
-function seatToSeatRow(seatNo, seatsPerRow) {
+function calcRow(seatNo, seatsPerRow) {
   let count = 0;
   for (let i = 0; i < seatsPerRow.length; i++) {
     count += seatsPerRow[i];
     if (seatNo > count) continue;
-    return { seat: seatNo, row: i + 1 };
+    return i + 1;
   }
   throw new Error("seatNo is out of range");
 }
 
-module.exports = seatToSeatRow;
+module.exports = calcRow;
