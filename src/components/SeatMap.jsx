6ee0fd7760eda6f53debb4b4e129c663ec713a96
Lisa Hansson <lisastina.hansson@gmail.com>
2021-06-11 10:15:34 +0200

    Changed onChange to onClick, and simplified theater size check
    Co-authored-by: Sebastian Mineur <sebastian.mineur@gmail.com>

--- src/components/SeatMap.jsx
+++ src/components/SeatMap.jsx
@@ -1,17 +1,21 @@
 import style from "../css/SeatMap.module.css";
 import { useState, useEffect } from "react";
 
-const SeatMap = (props) => {
+const SeatMap = (
+  {
+    /*  screening, selectedTickets, selectedSeats, setSelectedSeats  */
+  }
+) => {
   const [theater, setTheater] = useState(null);
 
   /* remove and get from booking props */
   const [selectedSeats, setSelectedSeats] = useState([]);
-  const tickets = 3;
+  const selectedTickets = 3;
   const screening = {
     occupiedSeats: [
       1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
     ],
-    theaterId: "60a6435192b1562b546900d5",
+    theaterId: "60a6435192b1562b546900d6",
   };
 
   /* fetch theaters and find right theater */
@@ -28,7 +32,7 @@ const SeatMap = (props) => {
   }, []);
 
   /* set selected seats state */
-  const handleChange = (e) => {
+  const handleSeatClick = (e) => {
     if (selectedSeats.includes(parseInt(e.target.value))) {
       setSelectedSeats(
         selectedSeats.filter((seat) => seat !== parseInt(e.target.value))
@@ -36,7 +40,7 @@ const SeatMap = (props) => {
       return;
     }
     /* prevent selection of more seats than tickets */
-    if (selectedSeats.length >= tickets) {
+    if (selectedSeats.length >= selectedTickets) {
       e.preventDefault();
       return;
     }
@@ -58,7 +62,7 @@ const SeatMap = (props) => {
               type="checkbox"
               className={style.seat}
               value={seatNumber}
-              onChange={handleChange}
+              onClick={handleSeatClick}
               /* if seat is occupied, make it disabled */
               disabled={screening.occupiedSeats.includes(seatNumber)}
             />
@@ -81,7 +85,8 @@ const SeatMap = (props) => {
       {theater && (
         <div
           className={`${style.seatMap} ${
-            theater.name === "Royal" && style.largeTheater
+            theater.seatsPerRow[theater.seatsPerRow.length - 1] > 11 &&
+            style.largeTheater
           }`}
         >
           {/* Simplify seatDescs?? */}
@@ -100,7 +105,7 @@ const SeatMap = (props) => {
             </div>
           </div>
           <hr />
-          <div className={style.seats}>{generateSeats().map((row) => row)}</div>
+          <div className={style.seats}>{generateSeats()}</div>
         </div>
       )}
     </div>
