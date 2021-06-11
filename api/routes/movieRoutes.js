ab76cabddf0c1e61e25599aa43511fc412f5b65c
Sebastian Mineur <sebastian.mineur@gmail.com>
2021-06-11 11:59:25 +0200

    Create getValues on /movies/values in api

--- api/routes/movieRoutes.js
+++ api/routes/movieRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const movieController = require("../controllers/movieController");
 
+router.get("/values", movieController.getValues);
 router.get("/:movieId", movieController.getMovieById);
 router.get("/", movieController.getAllMovies);
 
