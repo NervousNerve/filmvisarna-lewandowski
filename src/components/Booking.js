e265043b035c5d6a957b79268d94400a27fd261e
Lisa Hansson <lisastina.hansson@gmail.com>
2021-06-11 15:26:32 +0200

    Display flex on wide screens

--- src/components/Booking.js
+++ src/components/Booking.js
@@ -124,65 +124,69 @@ const Booking = ({ movie }) => {
 
   return (
     <div className={styles.bookingWrapper}>
-      <div className={styles.pricetypeWrapper}>
-        <h4>
-          Adult
-          <p>Regular</p>
-        </h4>
+      <div className={styles.bookingContainer}>
+        <div>
+          <div className={styles.pricetypeWrapper}>
+            <h4>
+              Adult
+              <p>Regular</p>
+            </h4>
 
-        <div className="number-input">
-          <NumberInput updateValue={setAdult} />
+            <div className="number-input">
+              <NumberInput updateValue={setAdult} />
+            </div>
+          </div>
+          <div className={styles.pricetypeWrapper}>
+            <h4>
+              Child
+              <p>30% discount</p>
+            </h4>
+
+            <div className="number-input">
+              <NumberInput updateValue={setChild} />
+            </div>
+          </div>
+          <div className={styles.pricetypeWrapper}>
+            <h4>
+              Senior
+              <p>20% discount</p>
+            </h4>
+
+            <div className="number-input">
+              <NumberInput updateValue={setSenior} />
+            </div>
+          </div>
+
+          <div className={styles.selectWrapper}>
+            <div className="custom-select">
+              <select onChange={handleChange}>
+                <option value={""}>Date and time</option>
+                {screeningSchedule &&
+                  screeningSchedule.map((screening, i) => {
+                    return (
+                      <option value={screening._id} key={i}>
+                        {new Date(screening.date).toLocaleString("sv-SE", {
+                          timeZone: "Europe/Stockholm",
+                        })}
+                      </option>
+                    );
+                  })}
+              </select>
+              <span className="focus"></span>
+            </div>
+          </div>
         </div>
-      </div>
-      <div className={styles.pricetypeWrapper}>
-        <h4>
-          Child
-          <p>30% discount</p>
-        </h4>
 
-        <div className="number-input">
-          <NumberInput updateValue={setChild} />
-        </div>
+        {showSeatMap && (
+          <SeatMap
+            screening={screening}
+            theater={screening.theaterId}
+            setSelectedSeats={setSelectedSeats}
+            selectedSeats={selectedSeats}
+            selectedTickets={selectedTickets}
+          />
+        )}
       </div>
-      <div className={styles.pricetypeWrapper}>
-        <h4>
-          Senior
-          <p>20% discount</p>
-        </h4>
-
-        <div className="number-input">
-          <NumberInput updateValue={setSenior} />
-        </div>
-      </div>
-
-      <div className={styles.selectWrapper}>
-        <div className="custom-select">
-          <select onChange={handleChange}>
-            <option value={""}>Date and time</option>
-            {screeningSchedule &&
-              screeningSchedule.map((screening, i) => {
-                return (
-                  <option value={screening._id} key={i}>
-                    {new Date(screening.date).toLocaleString("sv-SE", {
-                      timeZone: "Europe/Stockholm",
-                    })}
-                  </option>
-                );
-              })}
-          </select>
-          <span className="focus"></span>
-        </div>
-      </div>
-
-      {showSeatMap && (
-        <SeatMap
-          screening={screening}
-          theater={screening.theaterId}
-          setSelectedSeats={setSelectedSeats}
-          selectedSeats={selectedSeats}
-          selectedTickets={selectedTickets}
-        />
-      )}
 
       <p className={styles.feedback}>{feedback}</p>
 
