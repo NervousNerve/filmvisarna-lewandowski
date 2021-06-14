c0e06f5f54ea37e88f2e869e6c779e160db307b6
Saga <sagaswahn@hotmail.com>
2021-06-14 16:22:27 +0200

    Only sending prop cancelBooking if !showPrevious

--- src/components/UserBookingItem.js
+++ src/components/UserBookingItem.js
@@ -1,25 +1,21 @@
 import styles from "../css/UserBookings.module.css";
 
-const UserBookingItem = ({ booking, showPrevious, cancelBooking }) => {
+const UserBookingItem = ({ booking, cancelBooking }) => {
+  const message =
+    "Are you sure you want to cancel your reservation? This action can not be undone.";
+
   return (
     <div className={styles.ticketContainer}>
       <div className={styles.removeItemContainer}>
         <h3 className={styles.title}>{booking.screeningId.movieId.title}</h3>
-        {!showPrevious && (
-          <button
-            className={styles.button}
-            onClick={() => {
-              if (
-                window.confirm(
-                  "Are you sure you want to cancel your reservation? This action can not be undone."
-                )
-              )
-                cancelBooking(booking._id);
-            }}
-          >
-            x
-          </button>
-        )}
+        <button
+          className={styles.button}
+          onClick={() => {
+            if (window.confirm(message)) cancelBooking(booking._id);
+          }}
+        >
+          x
+        </button>
       </div>
       <h4 className={styles.noBottomMargin}>
         Theater: {booking.screeningId.theaterId.name}
