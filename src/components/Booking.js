141b214d3a67fd45f74b2d58ca4a6ee0b1cd7ae3
Lisa Hansson <lisastina.hansson@gmail.com>
2021-06-15 10:12:27 +0200

    Create own seats component with selected/booked seats

--- src/components/Booking.js
+++ src/components/Booking.js
@@ -3,6 +3,7 @@ import { useHistory } from "react-router-dom";
 import NumberInput from "./NumberInput";
 import styles from "../css/Booking.module.css";
 import SeatMap from "./SeatMap";
+import Seats from "./Seats";
 
 const Booking = ({ movie }) => {
   const history = useHistory();
@@ -190,17 +191,7 @@ const Booking = ({ movie }) => {
 
       <p className={styles.feedback}>{feedback}</p>
 
-      <div className={styles.selectedSeats}>
-        {selectedSeats.length > 0 &&
-          selectedSeats.sort().map((seat) => {
-            return (
-              <div className={styles.seatWrapper}>
-                <div className={styles.yourSeat}></div>
-                <p>seat {seat}</p>
-              </div>
-            );
-          })}
-      </div>
+      <Seats seats={selectedSeats} />
 
       <div className={styles.totalPrice}>
         <h4>Total: {totalPrice} SEK</h4>
